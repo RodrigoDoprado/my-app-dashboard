e98bdd3cbc9f6072340639acb93f051ddc4e3bcd
RodrigoDoprado <68282603+RodrigoDoprado@users.noreply.github.com>
2022-11-01 15:53:10 -0300

    Update Index/server

--- src/service/index.ts
+++ src/service/index.ts
@@ -3,7 +3,8 @@ import { getUserLocalStorage } from "../context/authProvider/util"
 
 export const Api = axios.create({
     // baseURL: "https://reqres.in/api",
-    baseURL: "http://localhost:5000",
+    //baseURL: "http://localhost:5000",
+baseURL:"https://typebackend.herokuapp.com/"
 })
 
 // Api.interceptors.request.use(
