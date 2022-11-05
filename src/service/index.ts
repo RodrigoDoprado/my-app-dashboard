4dfed98716ce91cf8e3c8adaa824845509670c2e
rodrigodoprado <rodrigodoprado1992@gmail.com>
2022-11-05 06:41:29 -0300

    page register

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,19 +1,6 @@
 import axios from "axios"
 
 export const Api = axios.create({
-    // baseURL: process.env.DB_URL,
-    baseURL: process.env.API_URL,
+    baseURL: "https://apiback-end.herokuapp.com/",
 })
-
-// Api.interceptors.request.use(
-//     (config) => {
-//         const user = getUserLocalStorage();
-//         config.headers.Autorization = user?.token
-
-//         return config;
-//     },
-//     (error) => {
-//         return Promise.reject(error)
-//     }
-// )
 
